4425fff95011bf296bcd0093107519bc281300f3
indiciez <justinmajura@gmail.com>
2024-10-09 10:49:39 +0300

    Add pagination and random number utilities
    Implemented helper functions for pagination and generating random number strings in `src/index.ts`. Updated dependencies in `package.json` and `package-lock.json`, including the addition of `@nestjs/jwt` and `dotenv`.

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,29 @@ export function getTokenFromHeaders(headers: { [key: string]: any }): string | n
     const authHeader: string | undefined = headers['authorization'];
     return authHeader ? authHeader.replace('Bearer ', '') : null;
 }
+
+export function getRandomNumberString(length: number): string {
+    let result: string = '';
+    const characters = '0123456789';
+    const charactersLength: number = characters.length;
+
+    for (let i: number = 0; i < length; i++)
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    return result;
+}
+
+export function paginate(page: number = 1, limit: number = 25, totalItems: number): {
+    skip: number;
+    take: number;
+    totalPages: number;
+    previousPage: number | null;
+    nextPage: number | null;
+} {
+    const take: number = limit > 0 ? +limit : 10;
+    const skip: number = page > 1 ? (page - 1) * take : 0;
+    const totalPages: number = Math.ceil(totalItems / take);
+    const previousPage: number | null = page > 1 ? page - 1 : null;
+    const nextPage: number | null = +page < +totalPages ? +page + 1 : null;
+
+    return { skip, take, previousPage, nextPage, totalPages };
+}
